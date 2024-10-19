e7624e26e7bf70e24b2740e9ca60dae40209821d
Bruno Petrolini <brunopetrolini@hotmail.com>
2024-10-19 11:56:21 -0300

    chore: updates shadcn theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,23 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import "./globals.css"
+
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "FSW Barber",
   description: "A única barbearia que você precisa",
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="pt-BR" className="dark">
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
-  );
+  )
 }
