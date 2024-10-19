d956f30bf9f4d5c0f167fc3cf9fcce6922680828
Bruno Petrolini <brunopetrolini@hotmail.com>
2024-10-19 11:51:02 -0300

    chore: adds shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
+"use client"
+
+import { Button } from "./_components/ui/button"
 
 export default function Home() {
   return (
-    <h1>Hello, FSW!</h1>
-  );
+    <>
+      <h1>Hello, FSW!</h1>
+      <p>
+        <Button>Click-me</Button>
+      </p>
+    </>
+  )
 }
